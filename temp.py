0fce8e6d40b9b729f826107b5700dfb2dd12f30a
Yutak Seo <yts@smartinside.ai>
2025-08-07 20:42:37 +0900

    25.08.07

--- temp.py
+++ temp.py
@@ -2,7 +2,7 @@ import numpy as np
 from InnerDeployment.GeneticAlgorithm_cuda import SensorGA
 # 1. 맵 데이터 불러오기 (예시: numpy 2D 배열)
 from __MAPS__.MapLoader import MapLoader  # 예시 함수
-input_map = np.array(MapLoader("map_100x100.bot").MAP)
+input_map = np.array(MapLoader("map_250x280.bot").MAP)
 
 
 # 2. 파라미터 설정
