f19d446c125679ed51502581bfab689b299c0828
Yutak Seo <yts@smartinside.ai>
2026-01-07 06:15:22 +0000

    cuda

--- main2.py
+++ main2.py
@@ -4,12 +4,16 @@ import numpy as np
 from cpuinfo import get_cpu_info
 from Tools.PlotTools import VisualTool
 from OuterDeployment.HarrisCorner import *
-from SensorModule import Sensor
-from SensorModule.coverage import *
+from SensorModule.Sensor_v2 import Sensor
 from Tools.MapLoader import MapLoader
 
 # 사용할 알고리즘
 from InnerDeployment.GeneticAlgorithm import SensorGA
 
+vis=VisualTool()
 
 map=MapLoader().load("map_280x250.bot")
+vis.showJetMap(map,"test Map")
+
+sensor = Sensor(map).deploy((50,50),20)
+vis.showJetMap(map,"test Map")
--- sensor.Dockerfile
+++ sensor.Dockerfile
@@ -1,4 +1,4 @@
-FROM nvidia/cuda:11.8.0-cudnn8-devel-ubuntu22.04
+FROM nvidia/cuda:12.8.0-cudnn-runtime-ubuntu22.04
 
 WORKDIR /workspace
 
